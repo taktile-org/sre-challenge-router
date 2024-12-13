d2d4c986aaf56693ba914dc4eace20808f8c9d65
Matei <code@mateimicu.com>
2024-12-13 10:55:18 +0200

    feat: accept all paths

--- python_router/app.py
+++ python_router/app.py
@@ -3,12 +3,13 @@ from fastapi import FastAPI
 import httpx
 
 import random
+from urllib.parse import urljoin
 
 app = FastAPI()
 
 
-@app.get("/")
-async def route():
+@app.get("/{path:path}")
+async def route(path: str):
     backends = [
             "http://backend_good:9000/",
             "http://backend_sometime_works:9000/",
@@ -17,6 +18,6 @@ async def route():
     ]
     async with httpx.AsyncClient() as client:
         backend = random.choice(backends)
-        response = await client.get(backend)
+        response = await client.get(urljoin(backend, path))
 
     return response.json().get("message")
